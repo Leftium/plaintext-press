c7846511815f023b4e60e84148061df74ebda6b4
Leftium <john@leftium.com>
2020-03-17 18:10:52 -0500

    Fetch name and text from locator.

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,15 @@
     import View from './view.svelte'
 
     TEST_URLS = [
-        ['View, embedded content', '/Hello+World']
-        ['View, embedded content with share UI', '/Hello+World?links=1']
+        ['View: urlencoded', '/Hello+World']
+        ['View: https', '/https://www.dropbox.com/s/0dh0fivnoi8am7z/README.taskpaper?dl=0']
+        ['View: lzstring', '/lzstring:DYLwzgLgTglgdgcwHQQIZgNYAdVYKZQD0AKgBYxgAEFlEpelOAnsAPaoAmlAZq1JajiVQkWIiSUyNGnQbM2nHnwFCR0eMiA']
+        ['View: urlencoded, w/ link UI ', '/Hello+World?links=1']
         ['','']
-        ['Main, bare', '/']
-        ['Main, embedded content', '/?content=Hello+World']
-        ['Main, embedded content, buffer', '/?content=Hello+World&buffer=new.taskpaper']
-        ['Main, buffer', '/?buffer=new.taskpaper']
+        ['Main: bare', '/']
+        ['Main: urlencoded', '/?content=Hello+World']
+        ['Main: urlencoded, buffer', '/?content=Hello+World&buffer=new.taskpaper']
+        ['Main: no content, buffer', '/?buffer=new.taskpaper']
     ]
 
     component = Main
@@ -17,8 +19,6 @@
     if location.pathname isnt '/'
         component = View
 
-    url = location.href
-    console.log location
 </script>
 
 
@@ -28,7 +28,6 @@
 <template lang=pug>
 hr
 h2 App.svelte starts here:
-h3 Url: {url}
 ul
     +each('TEST_URLS as tu')
         li: a(href='{tu[1]}' title='{tu[1]}') {tu[0]}
--- src/View.svelte
+++ src/View.svelte
@@ -1,9 +1,71 @@
 <script type='text/coffeescript'>
+import lzString from 'lz-string'
 
+parseLocator = (locatorString) ->
+    full = data = locatorString
+    type = 'urlencoded'
+    if locatorString? and matches = locatorString.match /([^:]*):([\s\S]*)/
+        [full, type, data] = matches
+
+    return { full, type, data }
+
+fetchText = (url) ->
+    if not url then throw 'Error: Either url or text parameter required.'
+
+    url = url.replace /www.dropbox.com/, 'dl.dropboxusercontent.com'
+    res = await fetch url
+
+    if res.status is 200
+        return res.text()
+    else
+        throw "#{res.status}: Error loading #{url}"
+
+fetchLocator = (locator) ->
+    name = text = ''
+    if not locator? then return { name, text }
+
+    try
+        switch locator.type
+            when 'local'
+                # Todo
+                text = 'TODO: Type local: not implemented, yet!'
+            when 'lzstring'
+                # Type lzstring should always have a filename
+                # separatd by a '/'.
+                data = lzString.decompressFromEncodedURIComponent locator.data
+                if matches = data.match ///([^/]*)/([\s\S]*)///
+                    [_, name, text] = matches
+                else
+                    text = data
+            when 'urlencoded'
+                # Type urlencoded locators nave no name; only text
+                # Use type lzstring to attach a name
+                text = locator.data.replace /\+/g, '%20'
+                text = decodeURIComponent text
+            else
+                # Handle types http, https, ftp, etc...
+                name = locator.data.split('/').pop()
+                text = fetchText locator.full
+
+    return { name, text }
+
+
+
+
+locator = parseLocator location.pathname[1..].replace(/\/+$/, '')
+{name, text} = fetchLocator locator
+console.log locator
 </script>
 
 <template lang=pug>
-h1 View
+    h1 View
+
+    h2 name {name}
+    h2 text
+    +await('text then text')
+        p {text}
+        +catch('error')
+            p {error}
 </template>
 
 <style>
